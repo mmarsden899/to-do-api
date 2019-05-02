23a5c126303d3fdc3a373b21dc53ee2c1b70cb28
mmarsden89 <mmarsden89@gmail.com>
2019-05-02 19:40:32 -0400

    Add created_at attribute to user and item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,6 +46,6 @@ class ItemsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def item_params
-      params.require(:item).permit(:title, :description, :user_id)
+      params.require(:item).permit(:title, :description, :user_id, :created_at)
     end
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,4 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description
+  attributes :id, :title, :description, :created_at
   has_one :user
 end
